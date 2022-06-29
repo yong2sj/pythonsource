f452cc4160e80c5f629b93916a7c201f17485e88
yongheelee <ekor11@daum.net>
2022-06-29 17:40:37 +0900

    파일 read/write

--- basic/io3.py
+++ basic/io3.py
@@ -1,54 +1,64 @@
-# csv 파일 입출력
-
+# csv 파일 입출력 (csv : 콤마형태로 분리된 파일)
 import csv
 
-# reader = csv.reader(f)
-# 헤더명 제거
-# next(reader)
+# with open("data/sample1.csv", "r") as f:
+#     reader = csv.reader(f)
+#     # 헤더명 제거
+#     next(reader)
 
-# print(reader)
-# print(type(reader))
-# print(type())
+#     print(reader)
+#     print(type(reader))
+#     print(type(dir(reader)))
+#     print()
+#     for c in reader:
+#         print(c)
 
 # with open("data/sample2.csv", "r") as f:
-#    reader = csv.reader(f, delimiter="|")
-#
-#    for c in reader:
-#       print(c)
-
+#     reader = csv.reader(f, delimiter="|")  # |를 ,로 바꿔줌...
+#     for c in reader:
+#         print(c)
 
 # csv ==> dict 형태로 읽어오기
 # with open("data/sample1.csv", "r") as f:
-#   reader = csv.DictReader(f)
-#
-#   for c in reader:
-#      print(c)  # {'번호': '1', '이름': '김정수' ....}
-#     for k, v in c.items():
-#        print(k, v)
-#   print()
+#     reader = csv.DictReader(f)
 
-# sample3 읽기
-# with open("data/sample3.csv", "r") as f:
-#    reader = csv.reader(f)
-#
-#    for c in reader:
-#        print(c)
+#     for c in reader:
+#         print(c)  # 딕셔너리 형태로 출력됨
+#         for k, v in c.items():
+#             print(k, v)
+#         print()
 
-# 1차원 리스트 csv파일로 저장
-# list1 = [1, 2, 3, 4, 5]
+# with open("data/sample3.csv", "r", encoding="utf-8") as f:
+#     reader = csv.reader(f)
+
+#     for c in reader:
+#         print(c)
+
+
+# 1차원 리스트를 csv 파일로 저장
+# list1 = [1, 2, 3, 4, 5]  # list1 = list(range(1,6))
 
 # with open("data/sample4.csv", "w") as f:
-#    wt = csv.writer(f)
+#     write = csv.writer(f)
 
-#    wt.writerow(list1)
+#     write.writerow(list1)  # writerow : 줄단위로 적겠다
 
+list1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18],
+]
 
-list1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+# 줄마다 엔터가 한번씩 들어가있다.
+# 엔터가 안들어가게 해주고 싶으면 newline=""
+with open("data/sample5.csv", "w", newline="") as f:
 
-with open("data/sample5.csv", "w") as f:
-    wt = csv.writer(f)
+    write = csv.writer(f)
 
     # for row in list1:
-    #    wt.writerow(row)
+    #     write.writerow(row)
 
-    wt.writerow(list1)
+    # write.writerows(list1) # 확인없이 한번에 넣을 때 사용
--- basic/io4.py
+++ basic/io4.py
@@ -1,6 +1,4 @@
 # json 읽고 쓰기
-
-
 import json
 
 data = (
@@ -8,40 +6,52 @@ data = (
 )
 
 # 문자열 형태를 json 로드
-json_data = json.loads(data)
+# json_data = json.loads(data)
 
-# print(type(json_data))
 # print(
-#    type(json_data),
-#    json_data["id"],
-#    json_data["language"],
-#    json_data["edition"],
-#   json_data["author"],
+#     type(json_data),
+#     json_data["id"],
+#     json_data["language"],
+#     json_data["edition"],
+#     json_data["author"],
 # )
 
-# data = (
-#    '{"id": "hong", "language": "python","edition": "3.9","author": "Guido van Rossum"}'
-# )
-#
+# data = {
+#     "id": "hong",
+#     "language": "python",
+#     "edition": "3.9",
+#     "author": "Guido van Rossum",
+# }
+
+# # 딕셔너리 형태를 불러올 때 사용
 # json_data = json.dumps(data)
-#
+
 # print(type(json_data))
 # print(json_data)
 
-# key, value
+# data = {
+#     "id": "hong",
+#     "language": "python",
+#     "edition": "3.9",
+#     "author": "Guido van Rossum",
+# }
+
+# key, value 추가
 # data["language"] = ["java", "script"]
-#
+
 # with open("data/test1.json", "w") as f:
-#    json.dump(data, f, indent=2)
+#     json.dump(data, f, indent=2)  # indent=2 : 들여쓰기 하겠다
 
 # with open("data/test1.json", "r") as f:
-#    json_data = json.load(f)
-#
-#    print(json_data)
+#     json_data = json.load(f)
 
-with open("data/users.json", "r") as f:
+#     print(json_data)
+
+with open("data/user.json", "r") as f:
     json_data = json.load(f)
 
     # print(json_data)
     for person in json_data:
-        print(person)
+        # print(person)  # 한사람씩 볼 수 있다.
+        for k, v in person.items():
+            print(k, v)
